ea7e6725c8a814ad92cd82645350168b6b43e5df
BankrahStudios <Bankrah289@gmail.com>
2025-07-21 22:02:58 +0000

    Initial commit: implemented advanced model relationships in Django

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
